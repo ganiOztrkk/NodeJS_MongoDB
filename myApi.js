1938fba71971fe971469790831eb6e0785f460c1
Gani Ozturk <ganioztrk@gmail.com>
2023-12-07 18:36:27 +0300

    NodeJS Api - MongoDb CRUD

--- myApi.js
+++ myApi.js
@@ -4,37 +4,61 @@
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//api metotlarımız -- get post put delete
 
-//ilk parametre, adres cubugunda apimizi nasıl cagıracagız bunu temsil ediyoruz.
-app.get("/api",(req,res) =>{
-    res.json({message : "api calisiyor"});
+const uri = "mongodb://localhost:27017/tododb";
+mongoose.connect(uri).then(res=> {
+    console.log("Database bağlantısı başarılı")
+}).catch(err=> {
+    console.log(err);
 });
 
-const todos = ["todo1", "todo2", "todo3"];
+const todoSchema = new mongoose.Schema({
+    work: String,
+    date: Date
+});
 
-app.get("/api/getAll", (req, res) =>{
+const Todo = mongoose.model("Todo", todoSchema);
+
+//API metotlarımız => GET, POST, PUT, DELETE
+
+app.get("/api", (req, res)=> {
+    res.json({message: "API çalışıyor"});
+});
+
+app.get("/api/getAll", async (req,res)=> {
+    const todos = await Todo.find().sort({date: -1});
     res.json(todos);
 });
 
-app.post("/api/save",(req, res)=>{
-    const {value} = req.body;
-    todos.push(req.body.value);
+app.post("/api/removeById", async (req,res)=> {
+    const {_id} = req.body;
+
+    await Todo.findByIdAndDelete(_id);
+
     res.json({});
 })
 
-app.get("/api/removeByIndex/:id", (req, res)=>{
-    const index = req.params.id;
-
-    todos.splice(index,1);
-    res.json(null);
+app.post("/api/save", async (req,res)=> {
+    const {value} = req.body;
+   
+    const todo = new Todo({work: value, date: new Date().setHours(new Date().getHours() + 15)});
+    await todo.save();
+    res.json({});
 })
 
-app.listen(7001,()=>{
+app.post("/api/update", async (req,res)=> {
+    const {_id, value} = req.body;
+    
+    await Todo.findByIdAndUpdate(_id, {work: value});
+    res.json({});
+})
+
+app.listen(7002,()=>{
     console.log("api ayakta");
 });
-
